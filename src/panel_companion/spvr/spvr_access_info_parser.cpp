2411fe97c93e267b04a909b7d48a16aa194da68c
Hector <hollow.ichigo.yoo@gmail.com>
2025-12-06 16:51:55 +0800

    update access info parser
    Signed-off-by: Hector <hollow.ichigo.yoo@gmail.com>

--- src/panel_companion/spvr/spvr_access_info_parser.cpp
+++ src/panel_companion/spvr/spvr_access_info_parser.cpp
@@ -12,53 +12,25 @@ using namespace nlohmann;
 namespace tc
 {
 
-    //{
-    //    "spvr_srv_config": {
-    //        "srv_id": "",
-    //        "srv_name": "Srv.Supervisor.01",
-    //        "srv_type": "spvr",
-    //        "srv_w3c_ip": "192.168.1.111",
-    //        "srv_working_port": 30500,
-    //        "srv_grpc_port": 0,
-    //        "srv_appkey": "ff785bd3031bc6cf920a782e50f43dcb"
-    //    },
-    //    "relay_srv_config": [
-    //        {
-    //            "srv_id": "",
-    //            "srv_name": "Srv.Relay.01",
-    //            "srv_type": "relay",
-    //            "srv_w3c_ip": "192.168.1.111",
-    //            "srv_working_port": 30600,
-    //            "srv_grpc_port": 40600,
-    //            "srv_appkey": "ff785bd3031bc6cf920a782e50f43dcb"
-    //        }
-    //    ]
-    //}
+    // "spvr_srv_config": {
+    //     "srv_name": "Srv.01",
+    //     "srv_w3c_ip": "127.0.0.1",
+    //     "srv_spvr_port": 30500,
+    //     "srv_udp_broadcast_port": 30501,
+    //     "srv_relay_port": 30502,
+    //     "srv_appkey": "ff785bd3031bc6cf920a782e50f43dcb"
+    // }
 
     std::shared_ptr<SpvrAccessInfo> SpvrAccessInfoParser::ParseInfo(const std::string& info) {
         try {
             auto ac_info = std::make_shared<SpvrAccessInfo>();
             auto obj = json::parse(info);
-            {
-                auto spvr_obj = obj["spvr_srv_config"];
-                ac_info->spvr_config_.srv_name_ = spvr_obj["srv_name"].get<std::string>();
-                ac_info->spvr_config_.srv_type_ = spvr_obj["srv_type"].get<std::string>();
-                ac_info->spvr_config_.srv_w3c_ip_ = spvr_obj["srv_w3c_ip"].get<std::string>();
-                ac_info->spvr_config_.srv_working_port_ = spvr_obj["srv_working_port"].get<int>();
-                ac_info->spvr_config_.srv_appkey_ = spvr_obj["srv_appkey"].get<std::string>();
-            }
-
-            auto relay_objs = obj["relay_srv_config"];
-            for (const auto& relay_obj : relay_objs) {
-                auto relay_config = RelaySrvConfig{};
-                relay_config.srv_name_ = relay_obj["srv_name"].get<std::string>();
-                relay_config.srv_type_ = relay_obj["srv_type"].get<std::string>();
-                relay_config.srv_w3c_ip_ = relay_obj["srv_w3c_ip"].get<std::string>();
-                relay_config.srv_working_port_ = relay_obj["srv_working_port"].get<int>();
-                relay_config.srv_appkey_ = relay_obj["srv_appkey"].get<std::string>();
-                ac_info->relay_configs_.push_back(relay_config);
-            }
-
+            auto spvr_obj = obj["spvr_srv_config"];
+            ac_info->spvr_config_.srv_name_ = spvr_obj["srv_name"].get<std::string>();
+            ac_info->spvr_config_.srv_w3c_ip_ = spvr_obj["srv_w3c_ip"].get<std::string>();
+            ac_info->spvr_config_.srv_spvr_port_ = spvr_obj["srv_spvr_port"].get<int>();
+            ac_info->spvr_config_.srv_relay_port_ = spvr_obj["srv_relay_port"].get<int>();
+            ac_info->spvr_config_.srv_appkey_ = spvr_obj["srv_appkey"].get<std::string>();
             return ac_info;
         }
         catch (std::exception& e) {
