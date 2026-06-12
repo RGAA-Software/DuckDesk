974173e6ebafa8250aac743d31813579ffc348c8
Kimi <kimi@example.com>
2026-06-12 20:58:57 +0800

    update
    Signed-off-by: Kimi <kimi@example.com>

--- build_targets.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-call "C:\Program Files\Microsoft Visual Studio\18\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
-cd /d D:\source\GoCloud\GammaRayPremium\build_official
-ninja GammaRayClientInner GammaRay tc_protection 2>&1
