93579469142681d5b72ece08464d32ea987d567a
Kimi <kimi@example.com>
2026-08-14 23:50:32 +0800

    vendor: tc_spvr_client @ 6adc50192247

---
+++ src/gr_deps/tc_spvr_client/spvr_user.h
@@ -0,0 +1,56 @@
+//
+// Created by RGAA on 31/10/2025.
+//
+
+#ifndef GAMMARAYPREMIUM_SPVR_USER_H
+#define GAMMARAYPREMIUM_SPVR_USER_H
+
+#include <string>
+#include <memory>
+#include <nlohmann/json.hpp>
+
+using namespace nlohmann;
+
+namespace spvr
+{
+
+    const std::string kUserId = "uid";
+    const std::string kUserName = "username";
+    const std::string kUserHashPassword = "hash_password";
+    const std::string kUserNewHashPassword = "new_hash_password";
+    const std::string kUserPassword = "password";
+    const std::string kUserAssigned = "assigned";
+    const std::string kUserCreatedTimestamp = "created_timestamp";
+    const std::string kUserUpdateTimestamp = "update_timestamp";
+    const std::string kUserDeleted = "deleted";
+    const std::string kUserAvatarPath = "avatar_path";
+    const std::string kUserAdministrator = "administrator";
+    const std::string kPage = "page";
+    const std::string kPageSize = "page_size";
+
+    class SpvrUser {
+    public:
+        // obj["data"]["xx"]
+        static std::shared_ptr<SpvrUser> FromJson(const std::string& json_str);
+        // obj["xx"]
+        static std::shared_ptr<SpvrUser> FromObj(const json& obj);
+        std::string AsJson();
+        std::string Dump();
+
+    public:
+        std::string uid_;
+        std::string username_;
+        std::string password_;
+        bool assigned_;
+        int64_t created_timestamp_ = 0;
+        int64_t updated_timestamp_ = 0;
+        bool deleted_ = false;
+        std::string avatar_path_;
+        bool administrator_;
+    };
+
+    using SpvrUserPtr = std::shared_ptr<SpvrUser>;
+
+}
+
+#endif //GAMMARAYPREMIUM_SPVR_USER_H
---
+++ src/gr_deps/tc_spvr_client/spvr_user_api.h
@@ -0,0 +1,83 @@
+//
+// Created by RGAA on 31/10/2025.
+//
+
+#ifndef GAMMARAYPREMIUM_SPVR_USER_API_H
+#define GAMMARAYPREMIUM_SPVR_USER_API_H
+
+#include <map>
+#include <string>
+#include <vector>
+#include <tuple>
+#include <memory>
+
+#include "tc_common_new/expected.h"
+#include "spvr_errors.h"
+#include "spvr_user.h"
+
+namespace spvr
+{
+
+    class SpvrUserApi {
+    public:
+        // register
+        static
+        tc::Result<SpvrUserPtr, SpvrApiError>
+        Register(const std::string& host,
+                 int port,
+                 const std::string& appkey,
+                 const std::string& username,
+                 const std::string& hash_password);
+
+        // login
+        static
+        tc::Result<SpvrUserPtr, SpvrApiError>
+        Login(const std::string& host,
+              int port,
+              const std::string& appkey,
+              const std::string& username,
+              const std::string& hash_password,
+              const std::string& device_id);
+
+        // logout
+        static
+        tc::Result<SpvrUserPtr, SpvrApiError>
+        Logout(const std::string& host,
+               int port,
+               const std::string& appkey,
+               const std::string& uid,
+               const std::string& hash_password);
+
+        // update
+        static
+        tc::Result<SpvrUserPtr, SpvrApiError>
+        Update(const std::string& host,
+               int port,
+               const std::string& appkey,
+               const std::string& uid,
+               const std::string& hash_password,
+               const std::map<std::string, std::string>& values);
+
+        static
+        tc::Result<SpvrUserPtr, SpvrApiError>
+        UpdatePassword(const std::string& host,
+                       int port,
+                       const std::string& appkey,
+                       const std::string& uid,
+                       const std::string& old_hash_password,
+                       const std::string& new_hash_password);
+
+        // update avatar
+        static
+        tc::Result<SpvrUserPtr, SpvrApiError>
+        UpdateAvatar(const std::string& host,
+                     int port,
+                     const std::string& appkey,
+                     const std::string& uid,
+                     const std::string& hash_password,
+                     const std::string& avatar_path);
+    };
+
+}
+
+#endif //GAMMARAYPREMIUM_SPVR_USER_API_H
