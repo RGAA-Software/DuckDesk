636e9c2e6cce4793cb0855a15f82e63dc0cca130
Kimi <kimi@example.com>
2026-09-18 22:17:04 +0800

    test: align restore verifier with schema

--- rust_server/px_backup/src/restore_executor.rs
+++ rust_server/px_backup/src/restore_executor.rs
@@ -1145,7 +1145,7 @@ mod tests {
         tools.create_fresh_database(&target).unwrap();
         tools.restore_archive(&target, &archive_path).unwrap();
         tools
-            .verify_restored_database(&target, deployment_id, 24)
+            .verify_restored_database(&target, deployment_id, 25)
             .unwrap();
         let command_log = fs::read_to_string(&log_path).unwrap();
         assert!(command_log.contains("--template=template0"));
@@ -1155,7 +1155,7 @@ mod tests {
 
         fs::write(&psql, b"tampered").unwrap();
         assert_eq!(
-            tools.verify_restored_database(&target, deployment_id, 24),
+            tools.verify_restored_database(&target, deployment_id, 25),
             Err(RestoreExecutionError::ToolIdentity)
         );
     }
